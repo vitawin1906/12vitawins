ba5f30f37a653b0ca031104be4907894089a4d10
chuikoffru <chuikoff.ru@gmail.com>
2026-01-14 00:25:24 +0300

    Refactoring

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/hooks/use-toast";
-import {Link, useNavigate, useSearchParams} from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { useRegisterMutation } from "@/store/api/domains/authApi";
 import { useAuthStore } from "@/stores/authStore";
 import { ArrowLeft, Loader2, Eye, EyeOff } from "lucide-react";
@@ -78,7 +78,7 @@ export default function Register() {
                 description: "Добро пожаловать в VitaWin",
             });
 
-            navigate("/dashboard");
+            navigate("/account");
         } catch (err: any) {
             console.error("Register error:", err);
 
